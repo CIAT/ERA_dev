7e36127481b577803506491196d9a7ddbe52a892
pstewarda <p.steward@cgiar.org>
2025-02-19 22:01:51 +0300

    updated to acdc test case

--- R/search/oa_simple_example.R
+++ R/search/oa_simple_example.R
@@ -17,22 +17,66 @@ p_load(data.table)
 
 source("https://raw.githubusercontent.com/CIAT/ERA_dev/main/R/search/era_oa_query.R")
 
-terms<-list(practice = c("fertilizer"),
-            geo=c("kenya","tanzania","malawi","africa"),
-            outcome = c("yield","profit margin","usd","efficiency"))
+outcomes_terms <- c(
+  "Benefit-cost analysis", "Break-even period", "Economic valuation",
+  "Economic impact", "Net present value", "Payback period", "Willingness to pay",
+  "Gross margin","Kilograms per hectare","Cost",
+  "Climate change", "Risk assessment", "Vulnerability assessment",
+  "Adaptive management", "Community awareness", "Drought resistance","Indigenous knowledge",
+  "Changing climate","Flood resistance", "Soil fertility", "Soil degradation", 
+  "Biodiversity","Soil degradation","Yield loss cost","Revenue","Yield",
+  "Food security", "Water availability", "Water stress"
+)
 
-results<-era_oa_query(search_terms=terms, 
-             from_date = "1900-01-01", 
-             to_date = "2024-01-01", 
-             continent = NULL, 
-             download = FALSE,
-             full = FALSE, 
-             max_char_limit = 4000
-             ) 
+practices_terms <- c(
+  "Agroforestry", "Evergreen agriculture", "Farmer managed natural regeneration",
+  "Silvopastoral systems", "Alley cropping", "Crop rotation", "Cover cropping",
+  "Integrated pest management", "No tillage","Minimum tillage", "Soil conservation",
+  "Adapted cultivar","Crop diversification","Crop rotation","Green manure", 
+  "Water harvesting", "Managed grazing", "Irrigation","Residue retention",
+  "Rotational grazing", "Intensive grazing", "Biological pest control", "Managed grazing",
+  "Organic fertilizers","Short duration grazing","Strip grazing"
+)
 
-results$meta_data
-results$results
-str(results$results)
+product_terms <- c(
+  "Maize", "Zea mays", "Common beans", "Phaseolus vulgaris", "Cattle", "Bos taurus",
+  "Bos indicus", "Bull", "Heifer", "Bovine", "Coffee", "Coffea arabica", "Coffea robusta")
+
+region_terms <- c(
+  "Belize", "Bolivia", "Brazil", "Colombia", "Costa Rica", "Cuba", "Dominica",
+  "Dominican Republic", "Ecuador", "El Salvador", "Grenada", "Guatemala",
+  "Guyana", "Haiti", "Honduras", "Jamaica", "Mexico", "Nicaragua", "Paraguay",
+  "Peru", "Saint Lucia", "Saint Vincent and the Grenadines", "Suriname",
+  "Venezuela", "Argentina", "Bahamas", "Barbados", "Chile", "Panama",
+  "Trinidad and Tobago", "Uruguay"
+)
+
+terms<-list(outcomes_terms,practices_terms,product_terms,region_terms)
+
+
+b<-build_search_string2 (search_terms=terms, 
+                      within_block_operators  = "OR",
+                      between_block_operators = "AND",
+                      quote_terms             = T,
+                      max_term                = 100,
+                      max_char_limit          = 4000)
+
+cat(b)
+
+
+
+results <- era_oa_query(
+  search_terms = terms,
+  from_date    = "1900-01-01",
+  to_date      = Sys.Date(),
+  download     = FALSE,
+  full         = FALSE,
+  quote_terms  = TRUE,
+  max_char_limit = 4000,
+  max_term = 129
+)
+
+cat(results$api_endpoint)
 
 # Save to clipboard on windows
 write.table(results$results,"clipboard-256000",row.names = F,sep="\t")
