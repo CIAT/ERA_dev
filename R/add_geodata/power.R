30cadc1d15f6b7d223a1d32732d680dba6138d07
pstewarda <p.steward@cgiar.org>
2025-03-02 21:05:25 +0800

    documentation improved

--- R/add_geodata/power.R
+++ R/add_geodata/power.R
@@ -1,154 +1,189 @@
-# This script downloads NASA POWER data for unique locations in ERA. 
-# Run ERA_dev/R/0_set_env.R
-# This script is also dependent on altitude data having been calculated for ERA locations:
-# Run ERA_dev/R/add_geodata/download_dem.R before executing this script
-# Functions used are here https://raw.githubusercontent.com/CIAT/ERA_dev/main/R/0_functions.R
+# This script downloads NASA POWER data for unique locations in ERA.
+# It depends on these scripts:
+#   - Run ERA_dev/R/0_set_env.R first.
+#   - Run ERA_dev/R/add_geodata/download_dem.R to calculate altitude data.
+#
+# The script proceeds through these major steps:
+#   0) Workspace set-up (loading packages, subsetting sites, merging altitude, etc.)
+#   1) Downloading POWER data using the NASA POWER API.
+#   2) Processing and aggregating the downloaded data (annual averages, long-term averages).
 
 # 0) Set-up workspace ####
+
   ## 0.1) Load packages & functions #####
-  p_load(terra,sf,data.table,RCurl,future,future.apply,progressr,pbapply)
+  # Load required packages using pacman::p_load for concise syntax.
+  p_load(terra, sf, data.table, RCurl, future, future.apply, progressr, pbapply)
   
+  # Source external R scripts for PET calculations and POWER data functions.
+  # These functions are hosted on GitHub and must be accessible.
   source("https://raw.githubusercontent.com/CIAT/ERA_dev/refs/heads/main/R/add_geodata/PETcalc.R")
   source("https://raw.githubusercontent.com/CIAT/ERA_dev/refs/heads/main/R/add_geodata/download_power.R")
   source("https://raw.githubusercontent.com/CIAT/ERA_dev/refs/heads/main/R/add_geodata/process_power.R")
-
-  ## 0.2) Subset era sites according to buffer distance #####
-  # Set max buffer allowed
-  SS<-era_locations[Buffer<50000]
-  pbuf_g<-era_locations_vect_g[era_locations_vect_g$Buffer<50000,]
   
-  ## 0.3) Merge altitude data ####
-  era_elevation<-fread(file.path(era_dirs$era_geodata_dir,"elevation.csv"))
-  altitude_data<-era_elevation[variable=="elevation" & stat=="mean",.(Site.Key,value)]
-  setnames(altitude_data,"value","Altitude")
+  ## 0.2) Subset ERA sites according to buffer distance #####
+  # Limit to sites with a buffer distance below 50,000 units.
+  SS <- era_locations[Buffer < 50000]
+  # Subset the spatial vector of ERA sites to those with buffer < 50000.
+  pbuf_g <- era_locations_vect_g[era_locations_vect_g$Buffer < 50000, ]
   
-  # Merge altitude with site buffer vector
-  pbuf_g<-merge(pbuf_g,altitude_data,by="Site.Key")
+  ## 0.3) Merge altitude data #####
+  # Read in elevation data from a CSV file located in the ERA geodata directory.
+  era_elevation <- fread(file.path(era_dirs$era_geodata_dir, "elevation.csv"))
+  # Extract mean elevation for each site (Site.Key) where the variable is "elevation".
+  altitude_data <- era_elevation[variable == "elevation" & stat == "mean", .(Site.Key, value)]
+  # Rename the 'value' column to 'Altitude' to match expected naming conventions.
+  setnames(altitude_data, "value", "Altitude")
+  
+  # Merge the altitude data with the site buffer spatial object based on 'Site.Key'.
+  pbuf_g <- merge(pbuf_g, altitude_data, by = "Site.Key")
   
   ## 0.3) Create POWER parameter object #####
-  # Need to incorporate correction for PS to PSC - so altitude data from physical layer needs to be read in here
-  parameters<-c(
-    SRad="ALLSKY_SFC_SW_DWN", # Insolation Incident on a Horizontal Surface - MJ/m^2/day
-    Rain="PRECTOT", # Precipitation - mm day-1
-    Pressure.Corrected="PSC", # Corrected Atmospheric Pressure (Adjusted For Site Elevation) *(see Site Elevation section below) - kPa - Throws an error!
-    Pressure="PS", # Surface Pressure - kPa
-    Specific.Humid="QV2M", # Specific Humidity at 2 Meters - kg kg-1
-    Humid="RH2M", # Relative Humidity at 2 Meters - %
-    Temp.Mean="T2M", # Temperature at 2 Meters - C
-    Temp.Max="T2M_MAX", # Maximum Temperature at 2 Meters - C
-    Temp.Min="T2M_MIN", # Minimum Temperature at 2 Meters - C
-    WindSpeed="WS2M" # Wind Speed at 2 Meters - m/s
+  # Define a named character vector mapping human-readable parameter names to the NASA POWER API codes.
+  # Note: There is a correction for PS to PSC (see comments below regarding Pressure).
+  parameters <- c(
+    SRad              = "ALLSKY_SFC_SW_DWN",   # Insolation Incident on a Horizontal Surface - MJ/m^2/day
+    Rain              = "PRECTOT",             # Precipitation - mm day-1
+    Pressure.Corrected= "PSC",                 # Corrected Atmospheric Pressure (Adjusted For Site Elevation) - kPa
+    Pressure          = "PS",                  # Surface Pressure - kPa
+    Specific.Humid    = "QV2M",                # Specific Humidity at 2 Meters - kg kg-1
+    Humid             = "RH2M",                # Relative Humidity at 2 Meters - %
+    Temp.Mean         = "T2M",                 # Temperature at 2 Meters - C
+    Temp.Max          = "T2M_MAX",             # Maximum Temperature at 2 Meters - C
+    Temp.Min          = "T2M_MIN",             # Minimum Temperature at 2 Meters - C
+    WindSpeed         = "WS2M"                 # Wind Speed at 2 Meters - m/s
   )
   
-  ## 0.5) Set power data start and end dates #####
-  date_start<-"1984-01-01"
-  date_end<-"2024-12-31"
+  ## 0.5) Set POWER data start and end dates #####
+  # Define the temporal window for data download.
+  date_start <- "1984-01-01"
+  date_end   <- "2024-12-31"
   
   ## 0.6) Set workers for parallel downloads ####
-  worker_n<-10
+  # Define the number of parallel workers for the download operation.
+  worker_n <- 10
   
 # 1) Download POWER ####
-  ## 1.1) Run download function ####
-  curlSetOpt(timeout = 190) # increase timeout if experiencing issues with slow connection
-  
-  # Need to create loop that incorporates updated dates
-  # Names for min and max were the wrong way around in the previous version of this function
-  # Split download and compilation functions
-  overwrite<-F
-    
-  dl_errors<-download_power(site_vect=pbuf_g,
-                 date_start=date_start,
-                 date_end=date_end,
-                 altitude_field="Altitude",
-                 save_dir=era_dirs$power_dir,
-                 parameters=parameters,
-                 user_name="CIAT", 
-                 id="Site.Key",
-                 verbose=F,
-                 attempts=3,
-                 worker_n=worker_n,
-                 overwrite=overwrite)
-  
-  # Check for incomplete downloads (files should be >900kb)
-  files <- list.files(era_dirs$power_dir,".csv",full.names = T)
-  file_sizes <- file.info(files)$size/10^3
-  hist(file_sizes)
-  
-  # 2.3) Recompile Data
-  files<-list.files(era_dirs$power_dir,".csv$",full.names = T)
-  
-  power_data<-process_power(files=files,
-                                parameters = parameters,
-                                rename = names(parameters),
-                                id_field= "Site.Key",
-                                altitude_field="Altitude",
-                                add_date=T,
-                                add_daycount = T,
-                                time_origin = time_origin,
-                                add_pet=T,
-                                altitude_data=altitude_data,
-                                worker_n=worker_n)
-  
-  save_file<-file.path(era_dirs$era_geodata_dir,paste0("POWER_",Sys.Date(),".parquet"))
-  arrow::write_parquet(power_data,save_file)
 
+  ## 1.1) Run download function ####
+  # Increase the RCurl timeout option to allow for slow connections.
+  curlSetOpt(timeout = 190)
+  
+  # Set the overwrite flag. Set to FALSE to avoid re-downloading files that already exist.
+  overwrite <- FALSE
+  
+  # Call the download_power function to fetch data from the NASA POWER API.
+  # This function uses parallel processing and progress reporting.
+  dl_errors <- download_power(
+    site_vect      = pbuf_g,           # Spatial object containing site information
+    date_start     = date_start,       # Start date for the download period
+    date_end       = date_end,         # End date for the download period
+    altitude_field = "Altitude",       # Field in pbuf_g containing altitude data
+    save_dir       = era_dirs$power_dir, # Directory where CSV files will be saved
+    parameters     = parameters,       # Parameter codes for the API
+    user_name      = "CIAT",           # Username for the NASA POWER API
+    id             = "Site.Key",       # Field used as the site identifier
+    verbose        = FALSE,            # Set to TRUE for detailed output during download
+    attempts       = 3,                # Number of retry attempts for failed downloads
+    worker_n       = worker_n,         # Number of parallel workers
+    overwrite      = overwrite         # Flag indicating whether to overwrite existing files
+  )
+  
+  # Check for incomplete downloads:
+  # List all CSV files in the download directory and calculate file sizes (in KB).
+  files <- list.files(era_dirs$power_dir, ".csv", full.names = TRUE)
+  file_sizes <- file.info(files)$size / 10^3
+  hist(file_sizes)  # Display a histogram to inspect file sizes (e.g., files should be >900kb).
+  
+  ## 1.2) Recompile Data ####
+  # List CSV files again (ensuring file names end with .csv exactly).
+  files <- list.files(era_dirs$power_dir, ".csv$", full.names = TRUE)
+  
+  # Process the downloaded CSV files into a consolidated dataset.
+  # The process_power function aggregates and processes the POWER data.
+  power_data <- process_power(
+    files         = files,
+    parameters    = parameters,
+    rename        = names(parameters),  # Rename columns using the names defined in parameters
+    id_field      = "Site.Key",
+    altitude_field= "Altitude",
+    add_date      = TRUE,
+    add_daycount  = TRUE,
+    time_origin   = time_origin,  # Must be defined in your environment
+    add_pet       = TRUE,
+    altitude_data = altitude_data,
+    worker_n      = worker_n
+  )
+  
+  # Save the processed data to a Parquet file, with today's date appended.
+  save_file <- file.path(era_dirs$era_geodata_dir, paste0("POWER_", Sys.Date(), ".parquet"))
+  arrow::write_parquet(power_data, save_file)
+  
 # 2) Calculate annual averages ####
+
   ## 2.1.1) Remove incomplete years ####
-  power_data<-power_data[,N:=.N,by=list(Site.Key,Year)][!N<365][,N:=NULL]
+  # For each Site.Key and Year, count the number of observations.
+  # Remove groups with fewer than 365 records (i.e., incomplete years).
+  power_data <- power_data[, N := .N, by = list(Site.Key, Year)][!N < 365][, N := NULL]
   
   ## 2.1.2) Calculate annual data ####
-  power_data_annual<-power_data[,list(
-                            Total.Rain=as.integer(sum(Rain)),
-                            Total.ETo=as.integer(sum(ETo)),
-                            S.Humid.Mean=round(mean(Specific.Humid),2),
-                            Humid.Mean=round(mean(Humid),2),
-                            Temp.Mean.Mean=round(mean(Temp.Mean),2),
-                            Mean.N.30.Days=as.integer(sum(Temp.Mean>30)),
-                            Mean.N.35.Days=as.integer(sum(Temp.Mean>35)),
-                            Temp.Max.Mean=round(mean(Temp.Max),2),
-                            Temp.Max=max(Temp.Max),
-                            Max.N.40.Days=as.integer(sum(Temp.Max>40)),
-                            Temp.Min.Mean=round(mean(Temp.Min),2),
-                            Temp.Min=min(Temp.Min)),by=.(Site.Key,Year)]
+  # For each Site.Key and Year, compute annual aggregates:
+  # - Total rainfall, total PET (ETo), mean humidity, temperature statistics, etc.
+  power_data_annual <- power_data[, list(
+    Total.Rain      = as.integer(sum(Rain)),
+    Total.ETo       = as.integer(sum(ETo)),
+    S.Humid.Mean    = round(mean(Specific.Humid), 2),
+    Humid.Mean      = round(mean(Humid), 2),
+    Temp.Mean.Mean  = round(mean(Temp.Mean), 2),
+    Mean.N.30.Days  = as.integer(sum(Temp.Mean > 30)),
+    Mean.N.35.Days  = as.integer(sum(Temp.Mean > 35)),
+    Temp.Max.Mean   = round(mean(Temp.Max), 2),
+    Temp.Max        = max(Temp.Max),
+    Max.N.40.Days   = as.integer(sum(Temp.Max > 40)),
+    Temp.Min.Mean   = round(mean(Temp.Min), 2),
+    Temp.Min        = min(Temp.Min)
+  ), by = .(Site.Key, Year)]
   
-  ## 2.1.3) Calculate LT Data ####
-  power_data_lt<-power_data_annual[,list(Total.Rain.Mean=mean(Total.Rain),
-                               Total.ETo.Mean=mean(Total.ETo),
-                               S.Humid.Mean=mean(S.Humid.Mean),
-                               Humid.Mean=mean(Humid.Mean),
-                               Temp.Mean.Mean=mean(Temp.Mean.Mean),
-                               Mean.N.30.Days=mean(Mean.N.30.Days),
-                               Mean.N.35.Days=mean(Mean.N.35.Days),
-                               Temp.Max.Mean=mean(Temp.Max.Mean),
-                               Temp.Max=mean(Temp.Max),
-                               Max.N.40.Days=mean(Max.N.40.Days),
-                               Temp.Min.Mean=mean(Temp.Min.Mean),
-                               Temp.Min=mean(Temp.Min),
-                               Total.Rain.sd=sd(Total.Rain),
-                               Total.ETo.sd=mean(Total.ETo),
-                               S.Humid.Mean.sd=sd(S.Humid.Mean),
-                               Humid.Mean.sd=sd(Humid.Mean),
-                               Temp.Mean.Mean.sd=sd(Temp.Mean.Mean),
-                               Mean.N.30.Days.sd=sd(Mean.N.30.Days),
-                               Temp.Max.Mean.sd=sd(Temp.Max.Mean),
-                               Temp.Max.sd=sd(Temp.Max),
-                               Max.N.40.Days.sd=sd(Max.N.40.Days),
-                               Temp.Min.Mean.sd=sd(Temp.Min.Mean),
-                               Temp.Min.sd=sd(Temp.Min)
-                               ),by=Site.Key ]
-
-
+  ## 2.1.3) Calculate LT (Long-Term) Data ####
+  # Compute long-term averages and standard deviations by Site.Key across years.
+  power_data_lt <- power_data_annual[, list(
+    Total.Rain.Mean    = mean(Total.Rain),
+    Total.ETo.Mean     = mean(Total.ETo),
+    S.Humid.Mean       = mean(S.Humid.Mean),
+    Humid.Mean         = mean(Humid.Mean),
+    Temp.Mean.Mean     = mean(Temp.Mean.Mean),
+    Mean.N.30.Days     = mean(Mean.N.30.Days),
+    Mean.N.35.Days     = mean(Mean.N.35.Days),
+    Temp.Max.Mean      = mean(Temp.Max.Mean),
+    Temp.Max           = mean(Temp.Max),
+    Max.N.40.Days      = mean(Max.N.40.Days),
+    Temp.Min.Mean      = mean(Temp.Min.Mean),
+    Temp.Min           = mean(Temp.Min),
+    Total.Rain.sd      = sd(Total.Rain),
+    Total.ETo.sd       = sd(Total.ETo),
+    S.Humid.Mean.sd    = sd(S.Humid.Mean),
+    Humid.Mean.sd      = sd(Humid.Mean),
+    Temp.Mean.Mean.sd  = sd(Temp.Mean.Mean),
+    Mean.N.30.Days.sd  = sd(Mean.N.30.Days),
+    Temp.Max.Mean.sd   = sd(Temp.Max.Mean),
+    Temp.Max.sd        = sd(Temp.Max),
+    Max.N.40.Days.sd   = sd(Max.N.40.Days),
+    Temp.Min.Mean.sd   = sd(Temp.Min.Mean),
+    Temp.Min.sd        = sd(Temp.Min)
+  ), by = Site.Key]
+  
   ## 2.1.4) Round numeric columns ####
-  n_dp<-2
+  # Define the number of decimal places to round to.
+  n_dp <- 2
   
+  # Identify numeric columns and round them in the long-term dataset.
   numeric_cols <- names(power_data_lt)[sapply(power_data_lt, is.numeric)]
   power_data_lt[, (numeric_cols) := lapply(.SD, round, n_dp), .SDcols = numeric_cols]
   
+  # Similarly, round numeric columns in the annual dataset.
   numeric_cols <- names(power_data_annual)[sapply(power_data_annual, is.numeric)]
   power_data_annual[, (numeric_cols) := lapply(.SD, round, n_dp), .SDcols = numeric_cols]
-    
-  ## 2.1.5) Save annual & LT datasets ####
-  arrow::write_parquet(power_data_annual,gsub(".parquet$","_annual.parquet",save_file))
-  arrow::write_parquet(power_data_lt,gsub(".parquet$","_ltavg.parquet",save_file))
   
-    
+  ## 2.1.5) Save annual & long-term datasets ####
+  # Write the annual and long-term aggregated data to separate Parquet files.
+  arrow::write_parquet(power_data_annual, gsub(".parquet$", "_annual.parquet", save_file))
+  arrow::write_parquet(power_data_lt, gsub(".parquet$", "_ltavg.parquet", save_file))
