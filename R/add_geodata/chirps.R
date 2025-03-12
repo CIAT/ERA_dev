881cc63e0bcf74515a92bfaea5d168d248e704b2
pstewarda <p.steward@cgiar.org>
2025-03-12 17:55:24 +0300

    finalization of chirps scripts

---
+++ R/add_geodata/chirps.R
@@ -0,0 +1,216 @@
+# First run R/0_set_env.R
+
+# 1) Set-up workspace ####
+  # 1.1) Load packages and source functions #####
+pacman::p_load(data.table,pbapply,future,future.apply,R.utils,progressr,RCurl,exactextractr,sf)
+curlSetOpt(timeout = 190) # increase timeout if experiencing issues with slow connection
+
+source("https://raw.githubusercontent.com/CIAT/ERA_dev/refs/heads/main/R/add_geodata/download_chirps.R")
+source("https://raw.githubusercontent.com/CIAT/ERA_dev/refs/heads/main/R/add_geodata/process_chirps.R")
+
+
+  # 1.2) Set parallel workers ####
+worker_n<-10
+
+# 2) Create index of file download paths ####
+chirps_index<-rbind(
+  data.table(dataset="chirps",
+             region="africa",
+             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/africa_daily/tifs/p05",
+             variable=c("chirps-v2.0"),
+             variable_dir=NA,
+             startyear=1981,
+             endyear=2023,
+             format="tif.gz",
+             timestep="daily",
+             resolution=5),
+  data.table(dataset="chirps",
+             region="africa",
+             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/africa_daily/tifs/p25",
+             variable=c("chirps-v2.0"),
+             variable_dir=NA,
+             startyear=1981,
+             endyear=2023,
+             format="tif.gz",
+             timestep="daily",
+             resolution=25),
+  data.table(dataset="chirps",
+             region="global",
+             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05",
+             variable=c("chirps-v2.0"),
+             variable_dir=NA,
+             startyear=1981,
+             endyear=2023,
+             format="tif.gz",
+             timestep="daily",
+             resolution=5),
+  data.table(dataset="chirps",
+             region="global",
+             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p25",
+             variable=c("chirps-v2.0"),
+             variable_dir=NA,
+             startyear=1981,
+             endyear=2023,
+             format="tif.gz",
+             timestep="daily",
+             resolution=25),
+  data.table(dataset="chirps",
+             region="global",
+             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p25",
+             variable=c("chirps-v2.0"),
+             variable_dir=NA,
+             startyear=1981,
+             endyear=2023,
+             format="days_p25.nc",
+             timestep="daily",
+             resolution=25),
+  data.table(dataset="chirps",
+             region="global",
+             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p05",
+             variable=c("chirps-v2.0"),
+             variable_dir=NA,
+             startyear=1981,
+             endyear=2023,
+             format="days_p05.nc",
+             timestep="daily",
+             resolution=5)
+)
+
+
+chirps_index<-rbindlist(lapply(1:nrow(chirps_index),FUN=function(i){
+  x<-data.frame(chirps_index[i])
+  file_paths<-create_chirps_paths(base_url=x$base_url,
+                                  format=x$format,
+                                  startyear=x$startyear,
+                                  endyear=x$endyear,
+                                  variable_dir=x$variable_dir,
+                                  variable=x$variable,
+                                  dataset=x$dataset)
+  
+  x$file_paths<-list(file_paths)
+  return(x)
+}))
+
+
+chirps_index[grepl("chirps",variable),variable:="prec"
+][,variable:=tolower(variable)
+][,variable_dir:=NULL
+][grepl("tif",format),format:="tif"
+][grepl("nc",format),format:="nc"
+][,base_url:=NULL]
+
+# 3) Download data ####
+download_chirps(chirps_index,
+                var="prec",
+                reg="global",
+                res=5,
+                type="tif",
+                startyear = 1981,
+                endyear = 2023,
+                save_dir = era_dirs$chirps_dir,
+                year_folder=F)
+
+# 4) Unzip gz files ####
+files<-list.files(era_dirs$chirps_dir,".gz$",full.names = T)
+
+# Setup parallel plan
+future::plan(multisession, workers = worker_n)
+handlers("progress")
+# Track progress
+with_progress({
+  p <- progressor(along = files)
+  results <- future_lapply(seq_along(files), function(i) {
+    gunzip(files[i], remove = TRUE)
+    p(message = sprintf("Processed file %d/%d", i, length(files)))
+  })
+})
+# Reset to sequential if needed
+future::plan(sequential)  
+
+  # 4.1) Check files are complete ####
+files<-list.files(era_dirs$chirps_dir,".tif$",full.names = T)
+dates<-gsub(".tif","",basename(files))
+file_prefix<-"chirps-v2.0."
+dates<-gsub(file_prefix,"",dates)
+dates<-as.Date(dates,"%Y.%m.%d")
+
+# Create sequence of all days in chirps timeseries download range
+dates_seq <- seq.Date(from = as.Date("1981-01-01"), by = "day", to = as.Date("2023-12-31"))
+
+# Check all days are included
+dates_seq[!dates_seq %in% dates]
+
+# 5) Process data ####
+
+  ## 5.1) Restructure CHIRPS ####
+  
+    ### 5.1.1) Create folders ####
+    save_dir<-file.path(era_dirs$chirps_dir,"restructured")
+    if(!dir.exists(save_dir)){
+      dir.create(save_dir)
+    }
+  
+    ### 5.1.2) Create continental bounding boxes ####
+    # Define bounding boxes as named vectors
+    bbox_africa <- c(lon_min = -25, lon_max = 63, lat_min = -36, lat_max = 38)
+    bbox_sam    <- c(lon_min = -120, lon_max = -30, lat_min = -36, lat_max = 38)
+    bbox_asia   <- c(lon_min = 80, lon_max = 155, lat_min = -36, lat_max = 38)
+    
+    # Combine into a list if needed
+    bounding_boxes <- list(
+      Africa = bbox_africa,
+      SouthCentralAmerica = bbox_sam,
+      SouthSoutheastAsia = bbox_asia
+    )
+  
+    ### 5.1.3) Restructure data ####
+    
+    for(i in 1:length(bounding_boxes)){
+      cat("Transposing ",names(bounding_boxes)[i],", bounding box ",i,"/",length(bounding_boxes),"\n")
+      transpose_chirps(data_dir=era_dirs$chirps_dir,
+                       save_dir=file.path(save_dir,names(bounding_boxes)[i]),
+                       crop_box=bounding_boxes[[i]],
+                       overwrite=F)
+    }
+  
+  ## 5.2) Extract CHIRPS ####
+  
+  terra::gdalCache(size=20000)
+  
+  extract_chirps(site_vect=era_locations_vect_g,
+                 id_field="Site.Key",
+                 data_dir=era_dirs$chirps_dir,
+                 save_dir=era_dirs$era_geodata_dir,
+                 round_digits=2,
+                 add_daycount = TRUE,
+                 time_origin=time_origin,
+                 max_cells_in_memory = 3e+08,
+                 overwrite=F)
+  
+    ### 5.2.1) Calculate Annual & LT Data ####  
+  # Load CHIRPS
+  CHIRPS<-arrow::read_parquet(file.path(era_dirs$era_geodata_dir,"chirps.parquet"))
+  
+  CHIRPS[,Year:=format(date[1],"%Y"),by=date]
+  
+  # Remove incomplete years
+  CHIRPS[,n:=.N,by=.(Site.Key,Year)][!n<365][,n:=NULL][,Year:=NULL]
+  
+  # Calculate annual data 
+  CHIRPS.Annual<-CHIRPS[,list(Total.Rain=sum(Rain)),by=c("Site.Key","Year")]
+  
+  # Calculate LT Data
+  CHIRPS.LT<-CHIRPS.Annual[,list(Total.Rain.mean=mean(Total.Rain),Total.Rain.sd=sd(Total.Rain)),by=c("Site.Key")]
+  
+  # Round results
+  round_digits<-2
+  CHIRPS.Annual[,Total.Rain:=round(Total.Rain,round_digits)]
+  CHIRPS.LT[,Total.Rain:=round(Total.Rain.mean,round_digits)
+  ][,Total.Rain.sd:=round(Total.Rain.sd,round_digits)]
+  
+  # Save results
+  arrow::write_parquet(CHIRPS.Annual,paste0(era_dirs$era_geodata_dir,"chirps_annual.parquet"))
+  arrow::write_parquet(CHIRPS.LT,paste0(era_dirs$era_geodata_dir,"chirps_ltavg.parquet"))
+  
+  
+  
--- R/add_geodata/download_chirps.R
+++ R/add_geodata/download_chirps.R
@@ -1,255 +1,164 @@
-# First run R/0_set_env.R
-
-# 0) Set-up workspace ####
-# 0.1) Load packages and create functions #####
-pacman::p_load(data.table,pbapply,future,future.apply,R.utils,progressr,RCurl)
-curlSetOpt(timeout = 190) # increase timeout if experiencing issues with slow connection
-
-# 0.2) Set parallel workers ####
-worker_n<-10
-
-# 1) Create functions ####
-
-  remove_trailing_slash <- function(path) {
-    if (stringr::str_ends(path, "/")) {
-      path <- stringr::str_sub(path, 1, -2)
-    }
-    return(path)
+#' Remove Trailing Slash from Path
+#'
+#' Removes a trailing slash ('/') from a given file or URL path, if present.
+#'
+#' @param path Character. Path string to clean.
+#'
+#' @importFrom stringr str_ends str_sub
+#'
+#' @return Character. Path without a trailing slash.
+#' @export
+remove_trailing_slash <- function(path) {
+  if (stringr::str_ends(path, "/")) {
+    path <- stringr::str_sub(path, 1, -2)
   }
-  
-  create_chirps_paths<-function(base_url,format,startyear,endyear,variable_dir,variable,dataset){
-    base_url<-remove_trailing_slash(path=base_url)
-    if(grepl("tif",format)){
-      file_paths <- data.table(date=seq.Date(from = as.Date(paste0(startyear, "-01-01")),
-                                            to = as.Date(paste0(endyear, "-12-31")),
-                                            by = "day")
-                              )[,year:=format(date,"%Y")]
-      
-      if(!is.na(variable_dir)){
-        file_paths<-file_paths[,file_path:=file.path(base_url,variable_dir,year,paste0(variable,".",gsub("-",".",date),".",format))
-                                  ][,file_path]
-      }else{
-        file_paths<-file_paths[,file_path:=file.path(base_url,year,paste0(variable,".",gsub("-",".",date),".",format))
-                   ][,file_path]
-      }
-      
-    }
-    
-    if(grepl(".nc",format)){
-        if(is.na(variable_dir)){
-          file_paths<-file.path(base_url,paste0(variable,".",startyear:endyear,".",format))
-        }else{
-          file_paths<-file.path(base_url,variable_dir,paste0(variable,".",startyear:endyear,".",format))
-        }
-    }
-    
-    if(exists("file_paths")){
-      return(file_paths)
-    }else{
-      return(NA)
-    }
-  }
-  
-  download_chirps <- function(chirps_index, var, reg, res, type, startyear, endyear, save_dir, worker_n,year_folder) {
-    # Filter files based on parameters
-    files <- unlist(chirps_index[variable == var & region == reg & resolution == res & format == type, file_paths])
-    files <- files[grepl(paste(startyear:endyear, collapse = "|"), files)]
-    
-    if(year_folder & type=="tif"){
-      local_path <- file.path(save_dir,basename(dirname(files)),basename(files))
-    }else{
-      local_path <- file.path(save_dir, basename(files))
-    }
-    
-    # Create directories
-    dirs<-unique(dirname(local_path))
-    for(DIR in dirs){
-    if(!dir.exist(dirname(DIR))){
-      dir.create(dirname(DIR),recursive = T)
-    }
-    }
-    
-    # Create a data frame with URLs and local paths
-    files_df <- data.frame(
-      url = files,
-      local_path = file.path(save_dir, basename(files)),
-      stringsAsFactors = FALSE
-    )
-    
-    # Check if files already exist
-    files_df$f_exists <- file.exists(files_df$local_path)
-    files_df[!files_df$f_exists, "f_exists"] <- file.exists(gsub(".tif.gz", ".tif", files_df$local_path))
-    
-    # Filter out existing files
-    files_df <- files_df[!files_df$f_exists, ]
-    
-    # Function to attempt download with retries
-    attempt_download <- function(url, local_path) {
+  return(path)
+}
 
-      attempts <- 3
-      success <- FALSE
-      for (i in 1:attempts) {
-        try({
-          download.file(url, local_path)
-          success <- TRUE
-          break
-        }, silent = TRUE)
-        if (!success && grepl(".tif.gz$", url)) {
-          url <- gsub(".tif.gz$", ".tif", url)
-        }
-      }
-      return(success)
-    }
+#' Create CHIRPS File Paths
+#'
+#' Generates complete URLs for downloading CHIRPS data files based on specified parameters.
+#'
+#' @param base_url Character. Base URL of the CHIRPS dataset.
+#' @param format Character. File format ('tif', 'tif.gz', 'nc').
+#' @param startyear Numeric. Start year for data retrieval.
+#' @param endyear Numeric. End year for data retrieval.
+#' @param variable_dir Character or NA. Subdirectory for the variable, if applicable.
+#' @param variable Character. Variable name (e.g., 'chirps').
+#' @param dataset Character. Dataset identifier (currently unused).
+#'
+#' @import data.table
+#'
+#' @return Character vector of file URLs.
+#' @export
+create_chirps_paths <- function(base_url, format, startyear, endyear, variable_dir, variable, dataset) {
+  
+  # Ensure base_url has no trailing slash
+  base_url <- remove_trailing_slash(path = base_url)
+  
+  # Generate daily file paths for TIF formats
+  if (grepl("tif", format)) {
+    file_paths <- data.table(date = seq.Date(from = as.Date(paste0(startyear, "-01-01")),
+                                             to = as.Date(paste0(endyear, "-12-31")),
+                                             by = "day"))[, year := format(date, "%Y")]
     
-    # Perform downloads with lapply or future_lapply
-    failed_downloads <- NULL
-    if (worker_n == 1) {
-      failed_downloads <- lapply(1:nrow(files_df), function(i) {
-        cat("Downloading file", i, "/", nrow(files_df), "\n")
-        success <- attempt_download(files_df$url[i], files_df$local_path[i])
-        if (!success) return(files_df[i, ])
-        return(NULL)
-      })
+    if (!is.na(variable_dir)) {
+      file_paths <- file_paths[, file_path := file.path(base_url, variable_dir, year,
+                                                        paste0(variable, ".", gsub("-", ".", date), ".", format))
+      ][, file_path]
     } else {
-      future::plan(multisession, workers = worker_n)
-      failed_downloads <- future.apply::future_lapply(1:nrow(files_df), function(i) {
-        success <- attempt_download(files_df$url[i], files_df$local_path[i])
-        if (!success) return(files_df[i, ])
-        return(NULL)
-      })
-      future::plan(sequential)
+      file_paths <- file_paths[, file_path := file.path(base_url, year,
+                                                        paste0(variable, ".", gsub("-", ".", date), ".", format))
+      ][, file_path]
     }
-    
-    # Combine and return failed downloads
-    failed_downloads <- do.call(rbind, failed_downloads)
-    return(failed_downloads)
   }
   
-# 2) Create index of file download paths ####
-  chirps_index<-rbind(
-  data.table(dataset="chirps",
-             region="africa",
-             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/africa_daily/tifs/p05",
-             variable=c("chirps-v2.0"),
-             variable_dir=NA,
-             startyear=1981,
-             endyear=2023,
-             format="tif.gz",
-             timestep="daily",
-             resolution=5),
-  data.table(dataset="chirps",
-             region="africa",
-             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/africa_daily/tifs/p25",
-             variable=c("chirps-v2.0"),
-             variable_dir=NA,
-             startyear=1981,
-             endyear=2023,
-             format="tif.gz",
-             timestep="daily",
-             resolution=25),
-  data.table(dataset="chirps",
-             region="global",
-             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p05",
-             variable=c("chirps-v2.0"),
-             variable_dir=NA,
-             startyear=1981,
-             endyear=2023,
-             format="tif.gz",
-             timestep="daily",
-             resolution=5),
-  data.table(dataset="chirps",
-             region="global",
-             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/tifs/p25",
-             variable=c("chirps-v2.0"),
-             variable_dir=NA,
-             startyear=1981,
-             endyear=2023,
-             format="tif.gz",
-             timestep="daily",
-             resolution=25),
-  data.table(dataset="chirps",
-             region="global",
-             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p25",
-             variable=c("chirps-v2.0"),
-             variable_dir=NA,
-             startyear=1981,
-             endyear=2023,
-             format="days_p25.nc",
-             timestep="daily",
-             resolution=25),
-  data.table(dataset="chirps",
-             region="global",
-             base_url="https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p05",
-             variable=c("chirps-v2.0"),
-             variable_dir=NA,
-             startyear=1981,
-             endyear=2023,
-             format="days_p05.nc",
-             timestep="daily",
-             resolution=5)
+  # Generate yearly file paths for NetCDF formats
+  if (grepl(".nc", format)) {
+    if (is.na(variable_dir)) {
+      file_paths <- file.path(base_url, paste0(variable, ".", startyear:endyear, ".", format))
+    } else {
+      file_paths <- file.path(base_url, variable_dir, paste0(variable, ".", startyear:endyear, ".", format))
+    }
+  }
+  
+  # Return paths or NA if not generated
+  if (exists("file_paths")) {
+    return(file_paths)
+  } else {
+    return(NA)
+  }
+}
+
+#' Download CHIRPS Data
+#'
+#' Downloads CHIRPS data files filtered by specified criteria, with optional parallel processing and error handling.
+#'
+#' @param chirps_index Data.table. Index table containing available CHIRPS file URLs.
+#' @param var Character. Variable to filter from index.
+#' @param reg Character. Region to filter from index.
+#' @param res Character. Spatial resolution.
+#' @param type Character. File format (e.g., 'tif', 'nc').
+#' @param startyear Numeric. Start year for downloading data.
+#' @param endyear Numeric. End year for downloading data.
+#' @param save_dir Character. Local directory for saving downloaded files.
+#' @param worker_n Numeric. Number of parallel workers (1 for sequential download).
+#' @param year_folder Logical. Whether to create separate year-based folders.
+#'
+#' @import data.table
+#' @import future
+#' @import future.apply
+#'
+#' @return Data.frame listing any failed downloads.
+#' @export
+download_chirps <- function(chirps_index, var, reg, res, type, startyear, endyear, save_dir, worker_n, year_folder) {
+  # Filter URLs based on input parameters
+  files <- unlist(chirps_index[variable == var & region == reg & resolution == res & format == type, file_paths])
+  files <- files[grepl(paste(startyear:endyear, collapse = "|"), files)]
+  
+  # Define local save paths
+  if (year_folder & type == "tif") {
+    local_path <- file.path(save_dir, basename(dirname(files)), basename(files))
+  } else {
+    local_path <- file.path(save_dir, basename(files))
+  }
+  
+  # Create necessary directories
+  dirs <- unique(dirname(local_path))
+  for (DIR in dirs) {
+    if (!dir.exists(DIR)) {
+      dir.create(DIR, recursive = TRUE)
+    }
+  }
+  
+  # Dataframe of URLs and local file paths
+  files_df <- data.frame(
+    url = files,
+    local_path = local_path,
+    stringsAsFactors = FALSE
   )
   
-
-  chirps_index<-rbindlist(lapply(1:nrow(chirps_index),FUN=function(i){
-    x<-data.frame(chirps_index[i])
-    file_paths<-create_chirps_paths(base_url=x$base_url,
-                                    format=x$format,
-                                    startyear=x$startyear,
-                                    endyear=x$endyear,
-                                    variable_dir=x$variable_dir,
-                                    variable=x$variable,
-                                    dataset=x$dataset)
-    
-    x$file_paths<-list(file_paths)
-    return(x)
-  }))
-
-
-  chirps_index[grepl("chirps",variable),variable:="prec"
-               ][,variable:=tolower(variable)
-                 ][,variable_dir:=NULL
-                   ][grepl("tif",format),format:="tif"
-                     ][grepl("nc",format),format:="nc"
-                       ][,base_url:=NULL]
+  # Check if files already exist
+  files_df$f_exists <- file.exists(files_df$local_path)
+  files_df[!files_df$f_exists, "f_exists"] <- file.exists(gsub(".tif.gz$", ".tif", files_df$local_path[!files_df$f_exists]))
   
-# 3) Download data
-  download_chirps(chirps_index,
-                  var="prec",
-                  reg="global",
-                  res=5,
-                  type="tif",
-                  startyear = 1981,
-                  endyear = 2023,
-                  save_dir = era_dirs$chirps_dir,
-                  year_folder=F)
+  # Filter files needing download
+  files_df <- files_df[!files_df$f_exists, ]
   
-# 4) Unzip gz files ####
-  files<-list.files(era_dirs$chirps_dir,".gz$",full.names = T)
+  # Helper function to download with retries
+  attempt_download <- function(url, local_path) {
+    attempts <- 3
+    success <- FALSE
+    for (i in 1:attempts) {
+      try({
+        download.file(url, local_path)
+        success <- TRUE
+        break
+      }, silent = TRUE)
+      if (!success && grepl(".tif.gz$", url)) {
+        url <- gsub(".tif.gz$", ".tif", url)
+      }
+    }
+    return(success)
+  }
   
-  # Setup parallel plan
-  future::plan(multisession, workers = worker_n)
-  handlers("progress")
-  # Track progress
-  with_progress({
-    p <- progressor(along = files)
-    results <- future_lapply(seq_along(files), function(i) {
-      gunzip(files[i], remove = TRUE)
-      p(message = sprintf("Processed file %d/%d", i, length(files)))
+  # Perform downloads sequentially or in parallel
+  if (worker_n == 1) {
+    failed_downloads <- lapply(1:nrow(files_df), function(i) {
+      cat("Downloading file", i, "/", nrow(files_df), "\n")
+      success <- attempt_download(files_df$url[i], files_df$local_path[i])
+      if (!success) return(files_df[i, ]) else return(NULL)
     })
-  })
-  # Reset to sequential if needed
-  future::plan(sequential)  
+  } else {
+    future::plan(multisession, workers = worker_n)
+    failed_downloads <- future.apply::future_lapply(1:nrow(files_df), function(i) {
+      success <- attempt_download(files_df$url[i], files_df$local_path[i])
+      if (!success) return(files_df[i, ]) else return(NULL)
+    })
+    future::plan(sequential)
+  }
   
-  # 4.1) Check files are complete ####
-  files<-list.files(era_dirs$chirps_dir,".tif$",full.names = T)
-  dates<-gsub(".tif","",basename(files))
-  file_prefix<-"chirps-v2.0."
-  dates<-gsub(file_prefix,"",dates)
-  dates<-as.Date(dates,"%Y.%m.%d")
-  
-  # Create sequence of all days in chirps timeseries download range
-  dates_seq <- seq.Date(from = as.Date("1981-01-01"), by = "day", to = as.Date("2023-12-31"))
-  
-  # Check all days are included
-  dates_seq[!dates_seq %in% dates]
-  
+  # Combine and report failed downloads
+  failed_downloads <- do.call(rbind, failed_downloads)
+  return(failed_downloads)
+}
