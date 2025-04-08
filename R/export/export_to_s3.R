7c433c8a5449e9fb45ec58a1a3bb4b0116780092
pstewarda <p.steward@cgiar.org>
2025-04-08 07:37:45 +0300

    upload package files to s3

--- R/export/export_to_s3.R
+++ R/export/export_to_s3.R
@@ -373,7 +373,37 @@ upload_files_to_s3(files = files,
     )
     
     s3$dir_ls(file.path(s3_bucket,"archive"))
-       }
+    }
+    # 1.2.5.1) (optional) all files in common_data/era/data/packaged ####
+    if(F){
+      s3_bucket<-era_dirs$era_packaged_s3
+      folder_local<-era_dirs$era_packaged_dir
+      
+      files<-list.files(folder_local,full.names = T,recursive=F,include.dirs = F)
+      
+      upload_files_to_s3(files = files,
+                         selected_bucket=s3_bucket,
+                         max_attempts = 3,
+                         overwrite=F,
+                         mode="public-read")
+      
+      # Files no-longer present locally
+      files_s3<-s3$dir_ls(s3_bucket)
+      files_s3<-files_s3[files_s3!="s3://digital-atlas/era/data/archive"]
+      files_2archive_from<-files_s3[!basename(files_s3) %in% basename(files)]
+      files_2archive_to<-gsub("/data/","/data/archive/",files_2archive_from)
+      
+      s3fs::s3_file_copy(
+        path = files_2archive_from,
+        new_path = files_2archive_to
+      )
+      
+      s3fs::s3_file_delete(
+        path = files_2archive_from
+      )
+      
+      s3$dir_ls(file.path(s3_bucket,"archive"))
+    }
 
 # 1.4) Upload environmental data ####
    # (optional) all files in common_data/era/geodata ####
