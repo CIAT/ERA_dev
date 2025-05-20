3013b3321f28edf80ce90e3f3f957e005dd9afba
pstewarda <p.steward@cgiar.org>
2025-05-20 12:14:03 +0300

    naming conventions modified to include date?

--- R/export/package_datasets.R
+++ R/export/package_datasets.R
@@ -31,12 +31,15 @@ file_list<-list()
 (file_list$era_agronomy_dm<-tail(list.files(era_dirs$era_masterdata_dir,"agronomic.*json",full.names = T),1))
 (file_list$era_master_codes<-tail(list.files(era_dirs$era_masterdata_dir,"era_master_codes.*json",full.names = T),1))
 
-versions<-data.frame(versions=unlist(lapply(file_list,basename)))
+
 file_list$version<-file.path(era_dirs$era_masterdata_dir,"era_agronomy_version.csv")
 fwrite(versions,file_list$version)
 
 # Create .tar.gz
-file_archive<-file.path(era_dirs$era_packaged_dir,paste0("era_agronomy_bundle.tar.gz"))
+file_archive<-file.path(era_dirs$era_packaged_dir,paste0("era_agronomy_bundle_",Sys.Date(),".tar.gz"))
+file_archive2<-file.path(era_dirs$era_packaged_dir,paste0("era_agronomy_bundle.tar.gz"))
+
+versions<-data.frame(versions=unlist(lapply(file_list,basename)),archived_name=file_archive2)
 
 # Set working directory temporarily to the files' location
 old_wd <- setwd(dirname(file_list[[1]]))
@@ -48,13 +51,18 @@ tar(
   compression = "gzip",
   tar = "internal"
 )
+
+tar(
+  tarfile = file_archive2,
+  files = basename(unlist(file_list)),
+  compression = "gzip",
+  tar = "internal"
+)
 # Reset working directory
 setwd(old_wd)
 
 # Remove version file
 unlink(file_list$version)
-
- 
 
 # 2) Livestock ####
 (file_list$era_livestock_dm<-tail(list.files(era_dirs$era_masterdata_dir,"skinny_cow.*RData",full.names = T),1))
@@ -65,6 +73,7 @@ jsonlite::write_json(livestock_dm,file_list$era_livestock_dm)
   ## Combine most recent data for livestock
 (file_list$era_comparisons<-tail(list.files(era_dirs$era_masterdata_dir,"era_compiled.*parquet",full.names = T),1))
 (file_list$era_master_codes<-tail(list.files(era_dirs$era_masterdata_dir,"era_master_codes.*json",full.names = T),1))
+
 
 # To do: Subset compiled data to livestock
 compiled_livestock<-arrow::read_parquet(file_list$era_comparisons)
@@ -77,12 +86,14 @@ arrow::write_parquet(compiled_livestock,x)
 
 file_list$era_comparisons<-x
 
-versions<-data.frame(versions=unlist(lapply(file_list,basename)))
 file_list$version<-file.path(era_dirs$era_masterdata_dir,"era_agronomy_version.csv")
 fwrite(versions,file_list$version)
 
 # Create .tar.gz
-file_archive<-file.path(era_dirs$era_packaged_dir,paste0("era_livestock_bundle.tar.gz"))
+file_archive<-file.path(era_dirs$era_packaged_dir,paste0("era_livestock_bundle_",Sys.Date(),".tar.gz"))
+file_archive2<-file.path(era_dirs$era_packaged_dir,paste0("era_livestock_bundle.tar.gz"))
+
+versions<-data.frame(versions=unlist(lapply(file_list,basename)),archived_name=file_archive2)
 
 # Set working directory temporarily to the files' location
 old_wd <- setwd(dirname(file_list[[1]]))
@@ -94,6 +105,15 @@ tar(
   compression = "gzip",
   tar = "internal"
 )
+
+tar(
+  tarfile = file_archive2,
+  files = basename(unlist(file_list)),
+  compression = "gzip",
+  tar = "internal"
+)
+
+
 # Reset working directory
 setwd(old_wd)
 
